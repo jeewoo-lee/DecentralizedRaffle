c941e729557a3a1817888f39fe5e03883bf38eb9
Jeewoo Lee <44099418+jeewoo-lee@users.noreply.github.com>
2022-06-30 16:25:42 +0900

    created staging tests

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -14,18 +14,43 @@ developmentChains.includes(network.name)
               /**
                * NFT Contract
                */
-
               const nftAddress = await raffleFactory.nftAddress()
               nftContract = await ethers.getContractAt("NFT", nftAddress)
 
               /**
                * create raffle contract for testing purposes later on.
                */
-
               await raffleFactory.createRaffle(ITEM_PRICE, INTERVAL)
               const theAddress = await raffleFactory.raffles(0)
               raffleContract = await ethers.getContractAt("Raffle", theAddress)
               owner = await raffleContract.i_owner()
-              console.log("Owner:", owner)
+              console.log("Owner:", owner.toString())
+              console.log("Raffle Address:", theAddress.toString())
+              const accounts = await ethers.getSigners()
+          })
+
+          describe("user experience", function () {
+              it("successfully gets winner after the user enters", async () => {
+                  console.log("Starting the test ...")
+                  await new Promise(async (resolve, reject) => {
+                      raffleContract.once("WinnerPicked", async () => {
+                          console.log("Winner Picked!")
+                          try {
+                              const winNum = await raffleContract.s_winNum()
+                              console.log(winNum.toString())
+                              assert(winNum.toNumber() != 0)
+                              console.log((await accounts[0].getBalance()).toString())
+                              resolve()
+                          } catch (e) {
+                              reject(e)
+                          }
+                          console.log("Entering Raffle...")
+                          const tx = await raffleContract.enterRaffle({ value: raffleEntranceFee })
+                          await tx.wait(1)
+                          console.log("Ok, time to wait...")
+                          console.log((await accounts[0].getBalance()).toString())
+                      })
+                  })
+              })
           })
       })
